4756bbda064b84eac09f2ce7f6ee8e45fde46526
taynaindia <taynab542@gmail.com>
2026-05-11 18:49:45 -0300

    String Conexão

---
+++ Lojinha/DAL/Dados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.DAL
+{
+    public class Dados
+    {
+        public static string StringConexao
+        {
+            get 
+            {
+                return "server = Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dblojinha;Integrated Security=True;Encrypt=True"
+            }
+        }
+    }
+}
--- Lojinha/Modelos/ClienteInformation.cs
+++ Lojinha/Modelos/ClienteInformation.cs
@@ -36,6 +36,6 @@ namespace Lojinha.Modelos
             set { _telefone = value; }
         }
            
-
+               
     }
 }
